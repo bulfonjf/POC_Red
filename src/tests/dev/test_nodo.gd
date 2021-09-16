a0a60ba85fa59b305883647cdc991ff1a55adac5
bulfonjf <bulfonjf@gmail.com>
2021-09-16 00:05:24 -0300

    Se agrega dev tests de nodo
    Este seria el nodo que se crea desde el cliente, por eso se usa gdscript. La idea es hacer TDD comenzando por el primer caso de uso que es que el cliente se conecte a un servidor, para ello se crea un Nodo (ya que el cliente seria un nodo de la red del servidor).

---
+++ src/tests/dev/test_nodo.gd
@@ -0,0 +1,20 @@
+extends "res://addons/gut/test.gd"
+
+const uuid = preload('res://tools/uuid.gd')
+
+func test_crear_nodo():
+	# Arrange
+	var nombre_nodo = "cliente"
+	var id_nodo = uuid.v4()
+	var ip_nodo = "192.168.0.1"
+	var puerto_nodo = "8000"
+
+	# Act
+	var nodo = Nodo.new(nombre_nodo, id_nodo, ip_nodo, puerto_nodo)
+
+	# Assert
+	assert_is(nodo, Nodo)
+	assert_eq(nodo.nombre, nombre_nodo)
+	assert_eq(nodo.id, id_nodo)
+	assert_eq(nodo.ip, ip_nodo)
+	assert_eq(nodo.puerto, puerto_nodo)
--- src/tests/dev/test_red.gd
+++
@@ -1,7 +0,0 @@
-extends "res://addons/gut/test.gd"
-
-func test_true():
-    assert_true(true)
-
-func test_false():
-    assert_true(false)
